4aec31a08214589fb7ff7eea7a69b8ee71ba27f2
jamm0316 <jamm0316@naver.com>
2025-09-15 16:37:12 +0900

    feat: AppRouter 컴포넌트 라우트 경로 확장
    1. 루트 경로에서 인증 상태에 따른 HomeRedirect 추가한다.
    2. OAuth 콜백 페이지 경로 및 라우트 추가한다.
    3. 보호 라우트를 적용하여 인증이 필요한 경로를 분리한다.
    
    Resolves #170

--- frontend/src/router/AppRouter.jsx
+++ frontend/src/router/AppRouter.jsx
@@ -6,15 +6,29 @@ import App from "/src/App.jsx";
 import {ROUTES} from "/src/router/routes.js";
 import TaskCreatePage from "/src/pages/TaskCreatePage.jsx";
 import LoginPage from "/src/pages/auth/LoginPage.jsx";
+import HomeRedirect from "/src/router/HomeRedirect.jsx";
+import OAuthCallback from "/src/pages/auth/OAuthCallback.jsx";
+import ProtectedRoute from "/src/router/ProtectedRoute.jsx";
 
 const AppRouter = () => {
   return (
     <Routes>
       <Route element={<App/>}>
-        <Route path={ROUTES.ROOT} element={<LoginPage/>}/>
-        {/*<Route path={ROUTES} element={<Dashboard/>}/>*/}
-        <Route path={ROUTES.PROJECT.CREATE} element={<ProjectCreatePage />}/>
-        <Route path={ROUTES.TASK.CREATE} element={<TaskCreatePage />}/>
+        {/* 루트에서 로그인 상태에 따라 분기 */}
+        <Route path={ROUTES.ROOT} element={<HomeRedirect/>}/>
+
+        {/* 로그인 페이지 */}
+        <Route path={ROUTES.AUTH.LOGIN} element={<LoginPage/>}/>
+
+        {/* OAuth 콜백 페이지 */}
+        <Route path={ROUTES.AUTH.CALLBACK} element={<OAuthCallback/>}/>
+
+        {/* 보호 라우트(로그인 필요) */}
+        <Route element={<ProtectedRoute/>}>
+          <Route path={ROUTES.DASHBOARD} element={<Dashboard/>}/>
+          <Route path={ROUTES.PROJECT.CREATE} element={<ProjectCreatePage/>}/>
+          <Route path={ROUTES.TASK.CREATE} element={<TaskCreatePage/>}/>
+        </Route>
       </Route>
     </Routes>
   );
