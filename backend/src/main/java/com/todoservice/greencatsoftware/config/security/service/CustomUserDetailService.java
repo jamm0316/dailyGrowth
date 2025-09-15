5539bf51b448c35036e8acaf3207f35c89e40c54
jamm0316 <jamm0316@naver.com>
2025-09-15 15:58:19 +0900

    feat: CustomUserDetailService 로깅 추가 및 예외 처리 강화
    1. `@Slf4j` 어노테이션 추가로 로깅 기능 구현한다.
    2. 사용자 조회 시 로깅 메시지 추가하여 디버깅 용이성을 강화한다.
    3. 예외 발생 시 오류 로그 추가로 문제 분석을 지원한다.
    
    Resolves #158

--- backend/src/main/java/com/todoservice/greencatsoftware/config/security/service/CustomUserDetailService.java
+++ backend/src/main/java/com/todoservice/greencatsoftware/config/security/service/CustomUserDetailService.java
@@ -3,6 +3,7 @@ package com.todoservice.greencatsoftware.config.security.service;
 import com.todoservice.greencatsoftware.domain.member.domain.entity.Member;
 import com.todoservice.greencatsoftware.domain.member.domain.port.MemberRepository;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -11,14 +12,19 @@ import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
+@Slf4j
 public class CustomUserDetailService  implements UserDetailsService {
     private final MemberRepository memberRepository;
 
     @Override
     public UserDetails loadUserByUsername(String userId) throws UsernameNotFoundException {
+        log.info("Attempting to load user by ID: {}", userId);
         return memberRepository.findById(Long.parseLong(userId))
                 .map(this::createUserDetails)
-                .orElseThrow(() -> new UsernameNotFoundException("해당 사용자를 찾을 수 없습니다."));
+                .orElseThrow(() -> {
+                    log.error("User not found for ID: {}", userId);
+                    return new UsernameNotFoundException("해당 사용자를 찾을 수 없습니다.");
+                });
     }
 
     private UserDetails createUserDetails(Member member) {
