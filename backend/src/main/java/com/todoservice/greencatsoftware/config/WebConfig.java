334a02fc215c9acc2f223a8cad99326fb8179a14
jamm0316 <jamm0316@naver.com>
2025-09-15 16:04:52 +0900

    refactor: WebConfig 클래스 리팩토링 및 보안 설정 제거
    1. WebConfig 클래스에서 불필요한 보안 관련 설정(SecurityFilterChain) 제거한다.
    2. CorsConfigurer 관련 코드 삭제하여 코드 간결성을 개선한다.
    3. WebMvcConfigurer 구현을 통해 CORS 설정의 확장 가능성을 유지한다.
    
    Resolves #159

--- backend/src/main/java/com/todoservice/greencatsoftware/config/WebConfig.java
+++ backend/src/main/java/com/todoservice/greencatsoftware/config/WebConfig.java
@@ -1,41 +1,8 @@
 package com.todoservice.greencatsoftware.config;
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.Customizer;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-@EnableWebSecurity
-public class WebConfig {
-
-    @Bean
-    public WebMvcConfigurer corsConfigurer() {
-        return new WebMvcConfigurer() {
-            @Override
-            public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/api/**")
-                        .allowedOrigins("http://localhost:5173")
-//                        .allowedOrigins("https://0b1715f11995.ngrok-free.app")
-                        .allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS")
-                        .allowedHeaders("*")
-                        .allowCredentials(true);
-            }
-        };
-    }
-
-    @Bean
-    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http
-                .csrf(csrf -> csrf.disable()) // CSRF 보호 비활성화
-                .cors(Customizer.withDefaults()) // CORS 설정 적용
-                .authorizeHttpRequests(auth -> auth
-                        .anyRequest().permitAll() // 모든 요청을 임시로 허용
-                );
-        return http.build();
-    }
+public class WebConfig implements WebMvcConfigurer{
 }
