c1cd6b5f95371b96a8b029aa2659a80ca772e0e7
jamm0316 <jamm0316@naver.com>
2025-10-03 15:04:21 +0900

    refactor: 불필요한 의존성 및 Import 제거
    1. TaskService, TaskFactory, TaskController에서 불필요한 Import 삭제한다.
    2. 사용되지 않는 클래스 및 의존성을 정리하여 코드 가독성을 개선한다.
    3. 유지보수성을 높이기 위해 불필요한 참조를 제거한다.
    
    Resolves #179

--- backend/src/main/java/com/todoservice/greencatsoftware/domain/task/presentation/TaskController.java
+++ backend/src/main/java/com/todoservice/greencatsoftware/domain/task/presentation/TaskController.java
@@ -2,11 +2,12 @@ package com.todoservice.greencatsoftware.domain.task.presentation;
 
 import com.todoservice.greencatsoftware.common.baseResponse.BaseResponse;
 import com.todoservice.greencatsoftware.common.enums.Status;
-import com.todoservice.greencatsoftware.domain.project.domain.entity.Project;
-import com.todoservice.greencatsoftware.domain.project.presentation.dto.ProjectFieldUpdateRequest;
 import com.todoservice.greencatsoftware.domain.task.application.TaskService;
 import com.todoservice.greencatsoftware.domain.task.domain.entity.Task;
-import com.todoservice.greencatsoftware.domain.task.presentation.dto.*;
+import com.todoservice.greencatsoftware.domain.task.presentation.dto.TaskCreateRequest;
+import com.todoservice.greencatsoftware.domain.task.presentation.dto.TaskDetailResponse;
+import com.todoservice.greencatsoftware.domain.task.presentation.dto.TaskFieldUpdateRequest;
+import com.todoservice.greencatsoftware.domain.task.presentation.dto.TaskSummaryResponse;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
