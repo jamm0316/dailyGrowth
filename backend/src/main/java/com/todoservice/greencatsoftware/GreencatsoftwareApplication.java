4e72b859bcafe06156bd776fb5b6286c2751984e
jamm0316 <jamm0316@naver.com>
2025-10-06 06:33:24 +0900

    feat: ConfigurationPropertiesScan 추가
    1. `@ConfigurationPropertiesScan` 애너테이션을 추가한다.
    2. 애플리케이션 설정 파일(`application.properties`)의 프로퍼티를 스캔하도록 설정한다.
    3. 설정 자동 로딩 기능을 강화한다.
    
    Resolves #209

--- backend/src/main/java/com/todoservice/greencatsoftware/GreencatsoftwareApplication.java
+++ backend/src/main/java/com/todoservice/greencatsoftware/GreencatsoftwareApplication.java
@@ -2,10 +2,12 @@ package com.todoservice.greencatsoftware;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication
 @EnableTransactionManagement
+@ConfigurationPropertiesScan
 public class GreencatsoftwareApplication {
 
 	public static void main(String[] args) {
