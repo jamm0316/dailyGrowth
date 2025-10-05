adc1c4ca3bf8ac459cb2bd7d02bbe3db06818548
jamm0316 <jamm0316@naver.com>
2025-10-06 05:34:17 +0900

    fix: baseurl https로 변경 1. 혼합콘텐츠 사용 오류 해결하기 2. 'http://dailygrowth.shop' -> 'https://dailygrowth.shop'
    Resolves #198

--- frontend/src/api/client.js
+++ frontend/src/api/client.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 export const API_BASE_URL = window.location.hostname === 'localhost'
   ? 'http://localhost:8080'
-  : 'http://dailygrowth.shop'
+  : 'https://dailygrowth.shop'
 
 const apiClient = axios.create({
   baseURL: API_BASE_URL,
