526b72c130a03780687eb210b51772ccbcc58173
jamm0316 <jamm0316@naver.com>
2025-10-07 06:53:24 +0900

    release: test Resolved: #209

--- backend/src/main/java/com/todoservice/greencatsoftware/GreencatsoftwareApplication.java
+++ backend/src/main/java/com/todoservice/greencatsoftware/GreencatsoftwareApplication.java
@@ -1,13 +1,15 @@
 package com.todoservice.greencatsoftware;
 
+import com.todoservice.greencatsoftware.config.OAuthProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication
 @EnableTransactionManagement
-@ConfigurationPropertiesScan
+@EnableConfigurationProperties(OAuthProperties.class)
 public class GreencatsoftwareApplication {
 
 	public static void main(String[] args) {
