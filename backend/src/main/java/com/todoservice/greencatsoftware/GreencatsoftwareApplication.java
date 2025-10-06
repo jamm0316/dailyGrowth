8c1a93c960e28745df8cb3913f48fe952d371e64
jamm0316 <jamm0316@naver.com>
2025-10-07 07:33:32 +0900

    refactor: OAuth 설정 어노테이션 변경
    1. `@Configuration`에서 `@Component`로 어노테이션 변경한다.
    2. `@EnableConfigurationProperties` 주석 처리된 코드 제거한다.
    3. OAuth 설정 클래스의 스프링 관리 방식 변경한다.
    
    Resolves #209

--- backend/src/main/java/com/todoservice/greencatsoftware/GreencatsoftwareApplication.java
+++ backend/src/main/java/com/todoservice/greencatsoftware/GreencatsoftwareApplication.java
@@ -9,7 +9,6 @@ import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication
 @EnableTransactionManagement
-//@EnableConfigurationProperties(OAuthProperties.class)
 public class GreencatsoftwareApplication {
 
 	public static void main(String[] args) {
--- backend/src/main/java/com/todoservice/greencatsoftware/config/OAuthProperties.java
+++ backend/src/main/java/com/todoservice/greencatsoftware/config/OAuthProperties.java
@@ -5,10 +5,11 @@ import lombok.Getter;
 import lombok.Setter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Component;
 
 import java.util.Map;
 
-@Configuration
+@Component
 @ConfigurationProperties(prefix = "oauth2")
 @Getter
 @Setter
