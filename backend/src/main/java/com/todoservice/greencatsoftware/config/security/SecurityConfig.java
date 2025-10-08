923906c602de0ebbe9e0bb03e9042dc6b6d7268c
jamm0316 <jamm0316@naver.com>
2025-10-09 02:20:28 +0900

    release: test
    Resolves #213

--- backend/src/main/java/com/todoservice/greencatsoftware/config/security/SecurityConfig.java
+++ backend/src/main/java/com/todoservice/greencatsoftware/config/security/SecurityConfig.java
@@ -55,7 +55,6 @@ public class SecurityConfig {
         return http.build();
     }
 
-
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
