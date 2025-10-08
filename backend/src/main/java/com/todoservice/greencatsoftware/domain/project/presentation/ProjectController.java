584cf2a3f40d25a463884d173e3a1f1ea8394bcd
jamm0316 <jamm0316@naver.com>
2025-10-09 05:44:42 +0900

    fix: AuthenticationPrincipal에서 springSecurity.userDetails.User 사용
    Resolves #225

--- backend/src/main/java/com/todoservice/greencatsoftware/domain/project/presentation/ProjectController.java
+++ backend/src/main/java/com/todoservice/greencatsoftware/domain/project/presentation/ProjectController.java
@@ -10,6 +10,8 @@ import com.todoservice.greencatsoftware.domain.project.presentation.dto.ProjectS
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -42,9 +44,10 @@ public class ProjectController {
     }
 
     @PostMapping("")
-    public BaseResponse<Project> createProject(@Valid @RequestBody ProjectCreateRequest newProjectDTO) {
-        log.info("[PROJECT_CREATE] incoming body: {}", newProjectDTO);
-        return new BaseResponse<>(projectService.createProject(newProjectDTO));
+    public BaseResponse<Project> createProject(
+            @AuthenticationPrincipal User user,
+            @Valid @RequestBody ProjectCreateRequest newProjectDTO) {
+        return new BaseResponse<>(projectService.createProject(user, newProjectDTO));
     }
 
     @PatchMapping("/{id}/field")
