97aba6e87919e99b4ffbbeb876e32bff5b8dc395
jamm0316 <jamm0316@naver.com>
2025-10-09 05:43:33 +0900

    fix: springSecurity.userDetails의 User 사용
    Resolves #225

--- backend/src/main/java/com/todoservice/greencatsoftware/domain/project/application/ProjectService.java
+++ backend/src/main/java/com/todoservice/greencatsoftware/domain/project/application/ProjectService.java
@@ -6,22 +6,22 @@ import com.todoservice.greencatsoftware.common.baseResponse.BaseResponseStatus;
 import com.todoservice.greencatsoftware.common.enums.Status;
 import com.todoservice.greencatsoftware.common.enums.Visibility;
 import com.todoservice.greencatsoftware.common.exception.BaseException;
-import com.todoservice.greencatsoftware.domain.color.entity.Color;
 import com.todoservice.greencatsoftware.domain.color.application.ColorService;
+import com.todoservice.greencatsoftware.domain.color.entity.Color;
 import com.todoservice.greencatsoftware.domain.project.domain.entity.Project;
 import com.todoservice.greencatsoftware.domain.project.domain.port.ProjectRepository;
 import com.todoservice.greencatsoftware.domain.project.domain.vo.Period;
-import com.todoservice.greencatsoftware.domain.project.presentation.dto.ProjectDetailResponse;
 import com.todoservice.greencatsoftware.domain.project.presentation.dto.ProjectCreateRequest;
+import com.todoservice.greencatsoftware.domain.project.presentation.dto.ProjectDetailResponse;
 import com.todoservice.greencatsoftware.domain.project.presentation.dto.ProjectFieldUpdateRequest;
 import com.todoservice.greencatsoftware.domain.project.presentation.dto.ProjectSummaryResponse;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.time.LocalDate;
-import java.util.LinkedHashMap;
 import java.util.List;
 
 @Slf4j
@@ -57,13 +57,8 @@ public class ProjectService {
     }
 
     @Transactional
-    public Project createProject(ProjectCreateRequest newProjectDTO) {
-        log.info("[PROJECT_CREATE] service.enter");
-        Project project = projectFactory.createProject(newProjectDTO);
-        log.info("[PROJECT_CREATE] entity.beforeSave id={}", project.getId());
-        Project save = projectRepository.save(project);
-        log.info("[PROJECT_CREATE] entity.afterSave");
-        return save;
+    public Project createProject(User user, ProjectCreateRequest newProjectDTO) {
+        return projectFactory.createProject(user, newProjectDTO);
     }
 
     @Transactional
