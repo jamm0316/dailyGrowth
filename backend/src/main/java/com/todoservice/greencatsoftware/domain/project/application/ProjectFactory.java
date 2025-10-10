2f04f1e885f5de0149f7c0237134508a63b374ba
jamm0316 <jamm0316@naver.com>
2025-10-10 11:33:17 +0900

    refactor: ProjectFactory 필요없는 로그 제거
    Resolves #225

--- backend/src/main/java/com/todoservice/greencatsoftware/domain/project/application/ProjectFactory.java
+++ backend/src/main/java/com/todoservice/greencatsoftware/domain/project/application/ProjectFactory.java
@@ -1,6 +1,5 @@
 package com.todoservice.greencatsoftware.domain.project.application;
 
-import com.todoservice.greencatsoftware.config.security.principal.CustomUser;
 import com.todoservice.greencatsoftware.domain.color.entity.Color;
 import com.todoservice.greencatsoftware.domain.color.application.ColorService;
 import com.todoservice.greencatsoftware.domain.member.application.MemberService;
@@ -9,11 +8,9 @@ import com.todoservice.greencatsoftware.domain.project.domain.entity.Project;
 import com.todoservice.greencatsoftware.domain.project.domain.vo.Period;
 import com.todoservice.greencatsoftware.domain.project.presentation.dto.ProjectCreateRequest;
 import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.stereotype.Component;
 
-@Slf4j
 @Component
 @RequiredArgsConstructor
 public class ProjectFactory {
@@ -21,28 +18,17 @@ public class ProjectFactory {
     private final ColorService colorService;
 
     public Project createProject(User user, ProjectCreateRequest request) {
-        log.info("[PROJECT_FACTORY]: factory.enter");
         long userId = Long.parseLong(user.getUsername());
-        log.info("[PROJECT_FACTORY]: user.Id:{}", userId);
         Member member = memberService.getMemberByIdOrThrow(userId);
-        log.info("[PROJECT_FACTORY]: getMemberId:{}", member.getId());
         Color color = colorService.getColorByIdOrThrow(request.colorId());
         Period period = Period.of(request.period().startDate(), request.period().endDate(),
-                                  request.period().actualEndDate());
-        Project project = (period.isNull())
+                request.period().actualEndDate());
+
+        return (period.isNull())
                 ? Project.create(color, member, request.name(), request.status(),
                 request.description(), request.isPublic(), request.visibility())
 
                 : Project.createWithPeriod(color, member, request.name(), request.status(),
                 period, request.description(), request.isPublic(), request.visibility());
-
-        log.info("[PROJECT_FACTORY]: project.id:{}, project.name:{}", project.getId(), project.getName());
-        return project;
-//                (period.isNull())
-//                ? Project.create(color, member, request.name(), request.status(),
-//                request.description(), request.isPublic(), request.visibility())
-//
-//                : Project.createWithPeriod(color, member, request.name(), request.status(),
-//                period, request.description(), request.isPublic(), request.visibility());
     }
 }
