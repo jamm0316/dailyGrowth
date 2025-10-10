c3232adfcde721e34ed5fcb3dd4de673b8778e43
jamm0316 <jamm0316@naver.com>
2025-10-10 18:26:33 +0900

    fix: taskSummary 조회 조건 추가
    1. Service의 taskSummary메서드 member로 조회 조건 추가
    
    Resolves #173

--- backend/src/main/java/com/todoservice/greencatsoftware/domain/task/application/TaskService.java
+++ backend/src/main/java/com/todoservice/greencatsoftware/domain/task/application/TaskService.java
@@ -33,8 +33,8 @@ public class TaskService {
         return taskRepository.findAll();
     }
 
-    public List<TaskSummaryResponse> summaryListTask() {
-        return taskRepository.summaryListTask();
+    public List<TaskSummaryResponse> summaryListTask(Long userId) {
+        return taskRepository.summaryListTask(userId);
     }
 
     public List<TaskSummaryResponse> todayListTask(LocalDate today) {
