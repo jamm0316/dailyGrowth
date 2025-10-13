fd74ef06088af721e0d91af63971a4a0b84c3cca
jamm0316 <jamm0316@naver.com>
2025-10-13 15:54:57 +0900

    refactor: 설정 관련 불필요한 import 제거
    1. OAuthProperties 및 관련 ConfigurationPropertiesScan, EnableConfigurationProperties import를 제거한다.
    2. 코드 간결성과 유지보수를 위해 사용되지 않는 import를 정리한다.
    
    Resolves #264

--- backend/src/main/java/com/todoservice/greencatsoftware/GreencatsoftwareApplication.java
+++ backend/src/main/java/com/todoservice/greencatsoftware/GreencatsoftwareApplication.java
@@ -1,10 +1,7 @@
 package com.todoservice.greencatsoftware;
 
-import com.todoservice.greencatsoftware.config.OAuthProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication
