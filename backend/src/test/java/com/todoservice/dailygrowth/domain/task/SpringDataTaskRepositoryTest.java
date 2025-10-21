f794f6d7fbe9b7712fddcf8b0bd2e6d6fe4d77fd
jamm0316 <jamm0316@naver.com>
2025-10-21 14:53:49 +0900

    test: ProjectType 필드 관련 테스트 로직 수정
    1. Project 생성 테스트에 ProjectType 필드를 추가한다.
    2. ProjectType 필드 값을 검증하는 로직을 강화한다.
    3. 기존 테스트의 일관성을 유지하며 수정한다.
    
    Resolves #275

--- backend/src/test/java/com/todoservice/dailygrowth/domain/task/SpringDataTaskRepositoryTest.java
+++ backend/src/test/java/com/todoservice/dailygrowth/domain/task/SpringDataTaskRepositoryTest.java
@@ -10,6 +10,7 @@ import com.todoservice.dailygrowth.domain.color.infrastructure.persistence.Sprin
 import com.todoservice.dailygrowth.domain.member.domain.entity.Member;
 import com.todoservice.dailygrowth.domain.member.infrastructure.persistence.SpringDataMemberJpaRepository;
 import com.todoservice.dailygrowth.domain.project.domain.entity.Project;
+import com.todoservice.dailygrowth.domain.project.domain.entity.ProjectType;
 import com.todoservice.dailygrowth.domain.project.infrastructure.persistence.SpringDataProjectJpaRepository;
 import com.todoservice.dailygrowth.domain.task.domain.entity.Task;
 import com.todoservice.dailygrowth.domain.task.domain.vo.Schedule;
@@ -71,6 +72,7 @@ public class SpringDataTaskRepositoryTest {
                 member,
                 "프로젝트B",
                 Status.PLANNING,
+                ProjectType.PERSONAL,
                 "프로젝트 입니다",
                 true,
                 Visibility.PRIVATE);
@@ -161,6 +163,7 @@ public class SpringDataTaskRepositoryTest {
                         member,
                         "새로운 프로젝트",
                         Status.PLANNING,
+                        ProjectType.PERSONAL,
                         "새로운 프로젝트 입니다",
                         true,
                         Visibility.TEAM));
