c5f122b495bd6dffbabcf2fccaf079e2d7f18720
jamm0316 <jamm0316@naver.com>
2025-10-22 10:16:24 +0900

    feat: ChallengeDetails VO 추가
    1. ChallengeDetails VO 클래스 생성 및 임베디드 설정을 추가한다.
    2. 챌린지 참여 인원 증가 및 감소 로직을 구현한다.
    3. 유효성 검증 로직 추가로 데이터 일관성을 유지한다.
    
    Resolves #277

---
+++ backend/src/main/java/com/todoservice/dailygrowth/domain/project/domain/vo/ChallengeDetails.java
@@ -0,0 +1,55 @@
+package com.todoservice.dailygrowth.domain.project.domain.vo;
+
+import com.todoservice.dailygrowth.common.baseResponse.BaseResponseStatus;
+import com.todoservice.dailygrowth.common.exception.BaseException;
+import jakarta.persistence.Embeddable;
+
+@Embeddable
+public record ChallengeDetails(
+        Integer participantCount,
+        Integer targetParticipants
+) {
+    public ChallengeDetails {
+        validationChallengeDetails(participantCount, targetParticipants);
+    }
+
+    private void validationChallengeDetails(int participantCount, int targetParticipants) {
+        if (targetParticipants <= 0) {
+            throw new BaseException(BaseResponseStatus.INVALID_CHALLENGE_CAPACITY);
+        }
+
+        if (participantCount <= 0) {
+            throw new BaseException(BaseResponseStatus.INVALID_CHALLENGE_PARTICIPANT_COUNT);
+        }
+
+        if (targetParticipants < participantCount) {
+            throw new BaseException(BaseResponseStatus.CANNOT_JOIN_FULL_CHALLENGE);
+        }
+    }
+
+    static public ChallengeDetails of(int targetParticipants) {
+        return new ChallengeDetails(1, targetParticipants);
+    }
+
+    public ChallengeDetails increaseParticipant() {
+        if (participantCount + 1 > targetParticipants) {
+            throw new BaseException(BaseResponseStatus.CANNOT_JOIN_FULL_CHALLENGE);
+        }
+        return new ChallengeDetails(participantCount + 1, targetParticipants);
+    }
+
+    public ChallengeDetails decreaseParticipant() {
+        if (participantCount - 1 < 0) {
+            throw new BaseException(BaseResponseStatus.INVALID_CHALLENGE_PARTICIPANT_COUNT);
+        }
+        return new ChallengeDetails(participantCount - 1, targetParticipants);
+    }
+
+    public boolean isFull() {
+        return participantCount == targetParticipants;
+    }
+
+    public boolean isNull() {
+        return participantCount == null && targetParticipants == null;
+    }
+}
