0f86a1802a34f83d41cefef7179ee29b356cc94c
jamm0316 <jamm0316@naver.com>
2025-10-22 10:16:11 +0900

    refactor: ProjectType 패키지 경로 변경
    1. ProjectType 클래스의 패키지를 entity에서 vo로 이동한다.
    2. Project 도메인의 구조적 일관성을 유지한다.
    3. 패키지 이동에 따른 코드 의존성을 점검한다.
    
    Resolves #277

--- backend/src/main/java/com/todoservice/dailygrowth/domain/project/domain/entity/ProjectType.java
+++
@@ -1,5 +0,0 @@
-package com.todoservice.dailygrowth.domain.project.domain.entity;
-
-public enum ProjectType {
-    PERSONAL, CHALLENGE
-}
---
+++ backend/src/main/java/com/todoservice/dailygrowth/domain/project/domain/vo/ProjectType.java
@@ -0,0 +1,5 @@
+package com.todoservice.dailygrowth.domain.project.domain.vo;
+
+public enum ProjectType {
+    PERSONAL, CHALLENGE
+}
