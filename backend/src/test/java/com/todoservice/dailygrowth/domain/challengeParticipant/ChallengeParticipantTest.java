cc716006cbc5fae37406c5942441ddcd8dab0dbd
jamm0316 <jamm0316@naver.com>
2025-10-30 17:50:30 +0900

    test: Challenge 및 ChallengeParticipant 테스트 코드 수정
    1. Challenge 관련 테스트 코드 추가 및 분리한다.
       - ChallengeTest 클래스 생성하여 고유 메서드 및 검증 로직 테스트 코드 작성한다.
    2. ChallengeParticipantTest에서 불필요한 테스트 제거 및 코드 정리한다.
       - Challenge 관련 테스트를 ChallengeTest로 이동한다.
       - 사용하지 않는 로직 및 중복 검증 메서드를 삭제한다.
    
    Resolves #290

--- backend/src/test/java/com/todoservice/dailygrowth/domain/challengeParticipant/ChallengeParticipantTest.java
+++ backend/src/test/java/com/todoservice/dailygrowth/domain/challengeParticipant/ChallengeParticipantTest.java
@@ -70,7 +70,7 @@ public class ChallengeParticipantTest {
         assertThat(challengeParticipant.getChallenge().getColor().getName()).isEqualTo("RED");
         assertThat(challengeParticipant.getChallenge().getChallengeDetails().challengeCategory()).isEqualTo(ChallengeCategory.WORKOUT);
         assertThat(challengeParticipant.getChallenge().getChallengeDetails().participantCount()).isEqualTo(1);
-        assertThat(challengeParticipant.getChallenge().getChallengeDetails().targetParticipants()).isEqualTo(3);
+        assertThat(challengeParticipant.getChallenge().getChallengeDetails().maxParticipant()).isEqualTo(3);
 
     }
 
@@ -88,33 +88,7 @@ public class ChallengeParticipantTest {
     }
 
     @Test
-    @DisplayName("검증 실패: project period가 null이면 BaseException 반환")
-    public void create_fail_validation_project_period_is_null() throws Exception {
-        //given
-        member = Member.create(
-                "member1@test.com",
-                OAuth2Provider.KAKAO,
-                "asevanoeqointqewr123",
-                "12345",
-                "null",
-                "testName");
-        Color color = Color.create("RED", "FF0000");
-        ChallengeDetails challengeDetails = ChallengeDetails.of(3, ChallengeCategory.WORKOUT);
-
-        //when
-        assertThatThrownBy(() -> Challenge.create(
-                color,
-                member,
-                "   나의 프로젝트    ",
-                Status.PLANNING,
-                null,
-                "   잘해보자구~",
-                challengeDetails))
-                .isInstanceOf(BaseException.class).hasMessage(BaseResponseStatus.CHALLENGE_PERIOD_UNDEFINED.getMessage());
-    }
-
-    @Test
-    @DisplayName("검증 실패: project period가 시작 전이면 BaseException 반환")
+    @DisplayName("검증 실패: joinedDateTime이 시작 날짜보다 이전이면 BaseException 반환")
     public void create_fail_validation_project_period_before_start () throws Exception {
         //given
         LocalDate changeStart = LocalDate.now().plusDays(1);
@@ -139,65 +113,5 @@ public class ChallengeParticipantTest {
         //when
         assertThatThrownBy(() -> ChallengeParticipant.create(challenge, member))
                 .isInstanceOf(BaseException.class).hasMessage(BaseResponseStatus.CHALLENGE_ENDED.getMessage());
-    }
-
-    @Test
-    @DisplayName("검증 실패: project status가 COMPLETE이면 BaseException 반환")
-    public void create_fail_validation_project_status_complete() throws Exception {
-        //given
-        challenge.changeStatus(Status.COMPLETED);
-
-        //when
-        assertThatThrownBy(() -> ChallengeParticipant.create(challenge, member))
-                .isInstanceOf(BaseException.class).hasMessage(BaseResponseStatus.CHALLENGE_COMPLETED.getMessage());
-    }
-
-    @Test
-    @DisplayName("수정 실패: ChallengeDetails의 participantCount가 targetParticipant보다 크면 BaseException 반환")
-    public void create_fail_validation_challenge_details() throws Exception {
-        //given
-        LocalDate startDate = LocalDate.now().minusDays(1);
-        LocalDate endDate = LocalDate.now().plusDays(1);
-        Color color = Color.create("RED", "FF0000");
-        Period period = Period.of(startDate, endDate, null);
-        ChallengeDetails challengeDetails = ChallengeDetails.of(1, ChallengeCategory.WORKOUT);
-
-        //when
-        Challenge newChallenge = Challenge.create(
-                color,
-                member,
-                "   나의 프로젝트    ",
-                Status.PLANNING,
-                period,
-                "   잘해보자구~",
-                challengeDetails);
-
-        //then
-        assertThatThrownBy(() -> newChallenge.getChallengeDetails().increaseParticipant())
-                .isInstanceOf(BaseException.class).hasMessage(BaseResponseStatus.CANNOT_JOIN_FULL_CHALLENGE.getMessage());
-    }
-    @Test
-    @DisplayName("수정 실패: ChallengeDetails의 participantCount가 0보다 작으면 BaseException 반환")
-    public void create_fail_validation_challenge_details_decrease() throws Exception {
-        //given
-        LocalDate startDate = LocalDate.now().minusDays(1);
-        LocalDate endDate = LocalDate.now().plusDays(1);
-        Color color = Color.create("RED", "FF0000");
-        Period period = Period.of(startDate, endDate, null);
-        ChallengeDetails challengeDetails = ChallengeDetails.of(1, ChallengeCategory.WORKOUT);
-
-        //when
-        Challenge newChallenge = Challenge.create(
-                color,
-                member,
-                "   나의 프로젝트    ",
-                Status.PLANNING,
-                period,
-                "   잘해보자구~",
-                challengeDetails);
-
-        //then
-        assertThatThrownBy(() -> newChallenge.getChallengeDetails().decreaseParticipant())
-                .isInstanceOf(BaseException.class).hasMessage(BaseResponseStatus.INVALID_CHALLENGE_PARTICIPANT_COUNT.getMessage());
     }
 }
